52415011d839ed25daf1c5738035d50d1f10d2a3
Runoah <rubengarciacubero@gmail.com>
2026-01-19 15:58:36 +0100

    Primer commit con frameworks instalados - Rubén

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">¡Tailwind Funciona!</h1>
+    </div>
+  )
+}
+export default App
