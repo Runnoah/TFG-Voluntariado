bf2fab0a41122f5394a9797a1c06f0ee3d6c0ba7
Runoah <rubengarciacubero@gmail.com>
2026-01-20 16:27:03 +0100

    Primera tabla en el modelo y actualización del .gitignore - Rubén

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'voluntariado',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voluntariado',
+        'USER': 'root',
+        'PASSWORD': 'root', 
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -115,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/voluntariado/models.py
+++ backend/voluntariado/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+
+    ROLES = [
+        ('voluntario', 'Voluntario'),
+        ('Administrador', 'Administrador de la plataforma'),
+    ]
+
+    rol = models.CharField(max_length=20, choices=ROLES, default='voluntario')
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.rol}"
+    
+
+class Pedanias(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Anuncios(models.Model):
+
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    fecha_evento = models.DateField()
+    pedanias_id = models.ForeignKey(Pedanias, on_delete=models.CASCADE, related_name='anuncios')
+    perfil_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='anuncios_creados')
+    
+    def __str__(self):
+        return self.titulo
